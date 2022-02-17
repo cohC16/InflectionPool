34f4c41745749fa77d67533ccaa0727a7cd0a2d7
Cassie <cassieroh@gmail.com>
2022-02-17 04:04:33 -0800

    updated installed apps with new dir

--- inflection/inflection/settings.py
+++ inflection/inflection/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'frontend.apps.FrontendConfig'
+    'inflection.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
